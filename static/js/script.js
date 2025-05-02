1fcceae1ae81d7294e08cec5826ee23770b65bf0
Gracious O. Asare <gpoa@bu.edu>
2025-05-02 17:46:51 -0400

    completed the section for the gallery slideshow and styling

--- static/js/script.js
+++ static/js/script.js
@@ -121,3 +121,36 @@ function changeActiveLink() {
 }
 
 window.addEventListener("scroll", changeActiveLink);
+
+// Gallery Slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// Slide every 5 seconds
+setInterval(function() {
+  plusSlides(1);
+}, 5000);
